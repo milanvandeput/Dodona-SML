9df9b1ad13a841d0aff6bb58cba5b64c48374210
milanvandeput <61200054+milanvandeput@users.noreply.github.com>
2023-09-18 09:33:14 +0200

    Update solution.py

--- tijdstippen/solution/solution.py
+++ tijdstippen/solution/solution.py
@@ -1,1 +1,15 @@
-print("Hello world")
+t1 = "1250"
+t2 = "1120"
+
+u1 = int(t1[0:2])
+u2 = int(t2[0:2])
+m1 = int(t1[2:4])
+m2 = int(t2[2:4])
+
+tv=(u2-u1)*60+(m2-m1)
+
+uv = (tv//60 + 24)%24
+mv = (tv%60 + 60)%60
+
+print(uv)
+print(mv)
